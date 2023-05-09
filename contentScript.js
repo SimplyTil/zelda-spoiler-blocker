4d62afd15c9db2c84db0401b8247a792f79cce53
Til L <git@simplytil.de>
2023-05-09 15:18:30 +0200

    added button to show or hide spoiler content; shows button only when spoiler is detected.

--- contentScript.js
+++ contentScript.js
@@ -1,11 +1,13 @@
 // Spoiler Array - Contains all the spoilers to be replaced
 const spoilers = [
-  { find: /zelda/i, replace: 'SPOILER' },
-  { find: /totk/i, replace: 'SPOILER' },
-  { find: /tears.?of.?the.?kingdom/i, replace: 'SPOILER' }
+  {find: /zelda/i, replace: 'SPOILER'},
+  {find: /totk/i, replace: 'SPOILER'},
+  {find: /tears.?of.?the.?kingdom/i, replace: 'SPOILER'}
 ];
 
+// Replace spoilers function that replaces spoilers in the text and hides the parent element
 function replaceSpoilers(node) {
+  let hasSpoilers = false; // Flag to track if at least one spoiler is found
   if (node.nodeType === Node.TEXT_NODE) {
     let text = node.textContent;
     for (const { find, replace } of spoilers) {
@@ -14,11 +16,17 @@ function replaceSpoilers(node) {
         const parentElement = node.parentElement;
         if (parentElement) {
           parentElement.style.display = 'none';
+          parentElement.classList.add('spoilered');
         }
+        hasSpoilers = true;
         break;
       }
     }
-  } else if (node.nodeType === Node.ELEMENT_NODE && node.nodeName !== 'SCRIPT' && node.nodeName !== 'STYLE') {
+  } else if (
+    node.nodeType === Node.ELEMENT_NODE &&
+    node.nodeName !== 'SCRIPT' &&
+    node.nodeName !== 'STYLE'
+  ) {
     if (node.nodeName === 'A') {
       for (const { find } of spoilers) {
         const regex = new RegExp(find, 'i');
@@ -27,23 +35,82 @@ function replaceSpoilers(node) {
             event.preventDefault();
             window.location.href = 'https://leckerer.link/h6ln2';
           });
+          node.classList.add('spoilered-link');
         }
       }
     }
     for (const childNode of node.childNodes) {
-      replaceSpoilers(childNode);
+      hasSpoilers = replaceSpoilers(childNode) || hasSpoilers; // Recursively check for spoilers in the child nodes and update the flag
     }
   }
+  return hasSpoilers; // Return the flag to indicate if at least one spoiler is found
 }
 
-replaceSpoilers(document.body);
+if (replaceSpoilers(document.body)) { // Check if there is at least one spoiler in the document body
+  // Add a button to show/hide the spoilers
+  const button = document.createElement('button');
+  button.innerText = 'Show/Hide Spoilers';
+  button.classList.add('spoiler-button');
+  button.addEventListener('click', () => {
+    const spoileredElements = document.querySelectorAll('.spoilered');
+    spoileredElements.forEach((element) => {
+      if (element.style.display === 'none') {
+        element.style.display = 'block';
+      } else {
+        element.style.display = 'none';
+      }
+    });
+  });
+
+  document.body.appendChild(button);
+}
 
 const observer = new MutationObserver((mutations) => {
   mutations.forEach((mutation) => {
     mutation.addedNodes.forEach((node) => {
-      replaceSpoilers(node);
+      if (replaceSpoilers(node)) { // Check if there is at least one spoiler in the added node
+        button.style.display = 'block'; // Show the button
+      }
     });
   });
 });
 
-observer.observe(document.body, { childList: true, subtree: true });
+observer.observe(document.body, {
+  childList: true,
+  subtree: true
+});
+
+// Add CSS styles to visually hide the spoilers and the spoiler button
+const styles = `
+.spoilered {
+  background-color: transparent !important;
+  position: relative !important;
+  z-index: 1 !important;
+  text-shadow: none !important;
+}
+
+.spoiler-button {
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  padding: 10px;
+  font-size: 16px;
+  background-color: black;
+  color: white;
+  border: none;
+  cursor: pointer;
+  z-index: 9999;
+}
+
+.spoilered-link {
+  text-decoration: underline dotted;
+}
+
+.spoilered-link:hover {
+  cursor: not-allowed;
+}
+`;
+
+const styleSheet = document.createElement('style');
+styleSheet.innerHTML = styles;
+document.head.appendChild(styleSheet);
